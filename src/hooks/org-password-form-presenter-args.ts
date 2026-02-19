1b7145ebefc72e03f757a6ec2504e9a18093fcd1
NOGUCHI Shoji <github.com@jpng.jp>
2026-02-19 13:22:46 +0900

    refactor(org-form): streamline presenter args builder

--- src/hooks/org-password-form-presenter-args.ts
+++ src/hooks/org-password-form-presenter-args.ts
@@ -2,12 +2,12 @@ import type { OrgEntryKindState } from "@/components/org/org-entry-kind";
 import type { OrgPasswordFormTranslations } from "@/hooks/org-password-form-translations";
 import type { OrgPasswordFormState } from "@/hooks/use-org-password-form-state";
 
-export interface OrgPasswordFormPresenterArgsInput {
+export type OrgPasswordFormPresenterArgsInput = {
   isEdit: boolean;
   entryKindState: OrgEntryKindState;
   translations: OrgPasswordFormTranslations;
   formState: OrgPasswordFormState;
-}
+};
 
 export type OrgPasswordFormPresenterArgs = OrgPasswordFormTranslations & {
   isEdit: boolean;
@@ -21,17 +21,10 @@ export function buildOrgPasswordPresenterArgs({
   translations,
   formState,
 }: OrgPasswordFormPresenterArgsInput): OrgPasswordFormPresenterArgs {
-  const { t, ti, tn, tcc, tpk, tGen } = translations;
-
   return {
     isEdit,
     entryKind: entryKindState.entryKind,
-    t,
-    ti,
-    tn,
-    tcc,
-    tpk,
-    tGen,
+    ...translations,
     formState,
   };
 }
